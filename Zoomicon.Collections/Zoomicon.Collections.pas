7555c2c079b0d42130411b2ef4c437e0b932084c
George Birbilis <birbilis@zoomicon.com>
2021-10-20 13:52:58 +0300

    Updated generic collection utilities

--- Zoomicon.Collections/Zoomicon.Collections.pas
+++ Zoomicon.Collections/Zoomicon.Collections.pas
@@ -2,6 +2,7 @@ unit Zoomicon.Collections;
 
 interface
   uses
+    SysUtils, //for TPredicate
     System.Generics.Collections, //for TList
     System.Rtti; //for RttiContext
 
@@ -9,34 +10,43 @@ type
 
   TListEx<T> = class(TList<T>)
     { GetRandom }
-    class function GetRandom(const list: TList<T>): T; overload;
+    class function GetRandom(const List: TList<T>): T; overload;
     function GetRandom: T; overload; virtual;
+
+    { GetAll }
+    class function GetAll(const Enum: TEnumerable<T>; const Predicate: TPredicate<T> = nil): TListEx<T>; overload;
+    function GetAll(const Predicate: TPredicate<T> = nil): TListEx<T>; overload;
   end;
 
+  //----------------------------------------------------------------------------
+
   TInterfaceListEx<T: IInterface> = class(TListEx<T>)
+    { GetAllOfInterface }
+    class function GetAllOfInterface<AInterface: IInterface>(const Enum: TEnumerable<T>; const OuterPredicate: TPredicate<T> = nil; const InnerPredicate: TPredicate<AInterface> = nil): TInterfaceListEx<AInterface>; overload;
+    function GetAllOfInterface<AInterface: IInterface>(const OuterPredicate: TPredicate<T> = nil; const InnerPredicate: TPredicate<AInterface> = nil): TInterfaceListEx<AInterface>; overload;
   end;
+
+  //----------------------------------------------------------------------------
 
   TObjectListEx<T: class> = class(TListEx<T>)
     { GetAllOfInterface }
-    class function GetAllOfInterface<AInterface: IInterface>(const list: TList<T>): TInterfaceListEx<AInterface>; overload;
-    function GetAllOfInterface<AInterface: IInterface>: TInterfaceListEx<AInterface>; overload;
+    class function GetAllOfInterface<AInterface: IInterface>(const Enum: TEnumerable<T>; const OuterPredicate: TPredicate<T> = nil; const InnerPredicate: TPredicate<AInterface> = nil): TInterfaceListEx<AInterface>; overload;
+    function GetAllOfInterface<AInterface: IInterface>(const OuterPredicate: TPredicate<T> = nil; const InnerPredicate: TPredicate<AInterface> = nil): TInterfaceListEx<AInterface>; overload;
 
     { GetAllOfClass }
-    class function GetAllOfClass<AClass: class>(const list: TList<T>): TObjectListEx<AClass>; overload;
-    function GetAllOfClass<AClass: class>: TListEx<AClass>; overload;
+    class function GetAllOfClass<AClass: class>(const Enum: TEnumerable<T>; const OuterPredicate: TPredicate<T> = nil; const InnerPredicate: TPredicate<AClass> = nil): TObjectListEx<AClass>; overload;
+    function GetAllOfClass<AClass: class>(const OuterPredicate: TPredicate<T> = nil; const InnerPredicate: TPredicate<AClass> = nil): TListEx<AClass>; overload;
   end;
 
 implementation
-  uses
-    SysUtils;
 
-{$REGION 'TListEx'}
+{$REGION 'TListEx' ------------------------------------------------------------}
 
 {$region 'GetRandom'}
 
-class function TListEx<T>.GetRandom(const list: TList<T>): T;
+class function TListEx<T>.GetRandom(const List: TList<T>): T;
 begin
-  with list do
+  with List do
     begin
     var c := Count;
     if c = 0 then
@@ -53,49 +63,101 @@ end;
 
 {$endregion}
 
-{$ENDREGION}
+{$region 'GetAll'}
 
-{$REGION 'TClassListEx'}
+class function TListEx<T>.GetAll(const Enum: TEnumerable<T>; const Predicate: TPredicate<T> = nil): TListEx<T>;
+begin
+  var ResultList := TListEx<T>.Create;
+  for var item in Enum do
+    if (@Predicate = nil) or Predicate(item) then
+      ResultList.Add(item);
+  result := ResultList;
+end;
+
+function TListEx<T>.GetAll(const Predicate: TPredicate<T> = nil): TListEx<T>;
+begin
+  result := {TListEx<T>.}GetAll(self);
+end;
+
+{$endregion}
+
+{$ENDREGION ...................................................................}
+
+{$REGION 'TInterfaceListEx' ------------------------------------------------------}
 
 {$region 'GetAllOfInterface'}
 
-class function TObjectListEx<T>.GetAllOfInterface<AInterface>(const list: TList<T>): TInterfaceListEx<AInterface>;
+class function TInterfaceListEx<T>.GetAllOfInterface<AInterface>(const Enum: TEnumerable<T>; const OuterPredicate: TPredicate<T> = nil; const InnerPredicate: TPredicate<AInterface> = nil): TInterfaceListEx<AInterface>;
 var
   itemAsAInterface: AInterface;
 begin
   var guid := TRttiInterfaceType(TRttiContext.Create.GetType(TypeInfo(AInterface))).GUID;
-  var listOfAInterface := TInterfaceListEx<AInterface>.Create;
-  for var item in list do
-    if Supports(item, guid, itemAsAInterface) then
-      listOfAInterface.Add(itemAsAInterface);
-  result := listOfAInterface;
+  var ListOfAInterface := TInterfaceListEx<AInterface>.Create;
+  for var item in Enum do
+    if Supports(item, guid, itemAsAInterface) and ((@OuterPredicate = nil) or OuterPredicate(item))  then
+      if (@InnerPredicate = nil) or InnerPredicate(itemAsAInterface) then
+        ListOfAInterface.Add(itemAsAInterface);
+  result := ListOfAInterface;
 end;
 
-function TObjectListEx<T>.GetAllOfInterface<AInterface>: TInterfaceListEx<AInterface>;
+function TInterfaceListEx<T>.GetAllOfInterface<AInterface>(const OuterPredicate: TPredicate<T> = nil; const InnerPredicate: TPredicate<AInterface> = nil): TInterfaceListEx<AInterface>;
 begin
-  result := {TClassListEx<T>.}GetAllOfInterface<AInterface>(self);
+  result := {TInterfaceListEx<T>.}GetAllOfInterface<AInterface>(Self, OuterPredicate, InnerPredicate);
+end;
+
+{$endregion}
+
+{$ENDREGION ...................................................................}
+
+{$REGION 'TObjectListEx' ------------------------------------------------------}
+
+{$region 'GetAllOfInterface'}
+
+class function TObjectListEx<T>.GetAllOfInterface<AInterface>(const Enum: TEnumerable<T>; const OuterPredicate: TPredicate<T> = nil; const InnerPredicate: TPredicate<AInterface> = nil): TInterfaceListEx<AInterface>;
+var
+  itemAsAInterface: AInterface;
+begin
+  var guid := TRttiInterfaceType(TRttiContext.Create.GetType(TypeInfo(AInterface))).GUID;
+  var ListOfAInterface := TInterfaceListEx<AInterface>.Create;
+  for var item in Enum do
+    if Supports(item, guid, itemAsAInterface) and ((@OuterPredicate = nil) or OuterPredicate(item))  then
+      if (@InnerPredicate = nil) or InnerPredicate(itemAsAInterface) then
+        ListOfAInterface.Add(itemAsAInterface);
+  result := ListOfAInterface;
+end;
+
+function TObjectListEx<T>.GetAllOfInterface<AInterface>(const OuterPredicate: TPredicate<T> = nil; const InnerPredicate: TPredicate<AInterface> = nil): TInterfaceListEx<AInterface>;
+begin
+  result := {TObjectListEx<T>.}GetAllOfInterface<AInterface>(Self, OuterPredicate, InnerPredicate);
 end;
 
 {$endregion}
 
 {$region 'GetAllOfClass'}
 
-class function TObjectListEx<T>.GetAllOfClass<AClass>(const list: TList<T>): TObjectListEx<AClass>;
+class function TObjectListEx<T>.GetAllOfClass<AClass>(const Enum: TEnumerable<T>; const OuterPredicate: TPredicate<T> = nil; const InnerPredicate: TPredicate<AClass> = nil): TObjectListEx<AClass>;
 begin
-  var listOfAClass := TObjectListEx<AClass>.Create;
-  for var item in list do
-    if item is AClass then
-      listOfAClass.Add(item as AClass);
-  result := listOfAClass;
+  var ListOfAClass := TObjectListEx<AClass>.Create;
+  for var item in Enum do
+    if (item is AClass) and ((@OuterPredicate = nil) or OuterPredicate(item)) then
+      begin
+      var itemAsAClass := item as AClass;
+      if (@InnerPredicate = nil) or InnerPredicate(ItemAsAClass) then
+        ListOfAClass.Add(itemAsAClass);
+      end;
+  result := ListOfAClass;
 end;
 
-function TObjectListEx<T>.GetAllOfClass<AClass>: TListEx<AClass>;
+type y = TPredicate<TObject>;
+type x = TPredicate<TListEx<Boolean>>;
+
+function TObjectListEx<T>.GetAllOfClass<AClass>(const OuterPredicate: TPredicate<T> = nil; const InnerPredicate: TPredicate<AClass> = nil): TListEx<AClass>;
 begin
-  result := {TClassListEx<T>.}GetAllOfClass<AClass>(self);
+  result := {TObjectListEx<T>.}GetAllOfClass<AClass>(Self, OuterPredicate, InnerPredicate);
 end;
 
 {$endregion}
 
-{$ENDREGION}
+{$ENDREGION ...................................................................}
 
 end.
