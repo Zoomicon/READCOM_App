0ec6a28f915cc588cdea5d322ef397907c25bfe4
birbilis@zoomicon.com <birbilis>
2022-01-19 11:20:50 +0200

    Added TreeViewHelpers to Zoomicon.Helpers

---
+++ Zooming.Helpers/Zoomicon.Helpers.FMX.TreeView/Zoomicon.Helpers.FMX.TreeView.TreeViewHelpers.pas
@@ -0,0 +1,50 @@
+unit Zoomicon.Helpers.FMX.TreeView.TreeViewHelpers;
+
+interface
+  uses
+    FMX.TreeView,
+    FMX.Types,
+    System.Types;
+
+  type
+
+    TTreeViewSearchHelper = class helper for TTreeView
+    public
+      function FindObject(const Value: TObject): TTreeViewItem;
+    end;
+
+    TTreeViewItemSearchHelper = class helper for TTreeViewItem
+    public
+      function FindObject(const Value: TObject): TTreeViewItem;
+    end;
+
+implementation
+
+function TTreeViewSearchHelper.FindObject(const Value: TObject): TTreeViewItem;
+begin
+  for var i := 0 to (Count - 1) do //Items is a 0-indexed array
+  begin
+    var foundItem := Items[i].FindObject(Items[i]); //note: that FindObject does depth-first search
+    if Assigned(foundItem) then
+      exit(foundItem);
+  end;
+
+  result := nil; //not found
+end;
+
+function TTreeViewItemSearchHelper.FindObject(const Value: TObject): TTreeViewItem;
+begin
+  if (TagObject = Value) then
+    exit(Self)
+  else
+    for var i := 0 to (Count - 1) do //Items is a 0-indexed array
+    begin
+      var foundItem := Items[i].FindObject(Value); //does recursion (depth-first)
+      if Assigned(foundItem) then //if not nil then has been found
+        exit(foundItem); //unwind stack and return the first one found
+    end;
+
+  result := nil; //not found
+end;
+
+end.
