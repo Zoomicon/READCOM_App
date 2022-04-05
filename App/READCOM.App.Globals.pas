e7a1d1de47100a6566736830c3c3c1e06cfa53ea
birbilis@zoomicon.com <George>
2022-04-06 02:28:43 +0300

    Updated version to 0.3.4

--- App/READCOM.App.Globals.pas
+++ App/READCOM.App.Globals.pas
@@ -10,7 +10,7 @@ resourcestring
   URL_HELP = 'https://github.com/Zoomicon/READCOM_App/wiki';
   URL_READCOM = 'https://www.read-com-eu.uma.es';
   STR_APP_TITLE = 'READ-COM: Reading Communities';
-  STR_APP_VERSION = '0.3.3'; //TODO: at https://docwiki.embarcadero.com/Libraries/Sydney/en/FMX.Forms.TApplication_Properties there is no way to get application version (as defined using Options.../Version Info in the IDE for the project of the app). For Windows and OS-X there's implementation here https://delphihaven.wordpress.com/2012/12/08/retrieving-the-applications-version-string/ and for iOS and Android here https://codeverge.com/embarcadero.delphi.firemonkey/getting-application-version-and/1050163 - One could make an application helper collecting all that instead of hard-coding version info here too
+  STR_APP_VERSION = '0.3.4'; //TODO: at https://docwiki.embarcadero.com/Libraries/Sydney/en/FMX.Forms.TApplication_Properties there is no way to get application version (as defined using Options.../Version Info in the IDE for the project of the app). For Windows and OS-X there's implementation here https://delphihaven.wordpress.com/2012/12/08/retrieving-the-applications-version-string/ and for iOS and Android here https://codeverge.com/embarcadero.delphi.firemonkey/getting-application-version-and/1050163 - One could make an application helper collecting all that instead of hard-coding version info here too
 
 type
   TGlobals = class(TDataModule)
