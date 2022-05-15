8c0d07b6afc60d7ffcbd34dca822b71d6fe88a33
birbilis@zoomicon.com <George>
2022-05-15 14:53:59 +0300

    Added tooling to generate Universal (both 32-bit and 64-bit) APK (can get from produced APKS [zip]) from AAB that is generated for Google Play Store at 64-bit build (with appropriate Delphi compiler option set to make AAB)

---
+++ App/Android64/GenerateUniversalAPKfromAAB.bat
@@ -0,0 +1,13 @@
+::See https://github.com/idzresearch/aab-to-apk-converter
+::Get latest bundletool.jar from https://github.com/google/bundletool/releases
+
+@set /p PWD_KEYSTORE="Keystore password: "
+@set /p PWD_KEY="Key password: "
+
+del READCOM_App.apks
+
+java -jar bundletool.jar build-apks --bundle=Release\READCOM_App\bin\READCOM_App.aab --output=READCOM_App.apks --overwrite --mode=universal --ks=%UserProfile%\OneDrive\Zoomicon\Android\Keystore.keystore --ks-pass=pass:%PWD_KEYSTORE% --ks-key-alias=readcom --key-pass=pass:%PWD_KEY%
+
+echo Open the .apks with a ZIP extractor (or rename to .zip) and get the universal .apk from there (should rename to READCOM_App.universal.apk)
+
+@pause
