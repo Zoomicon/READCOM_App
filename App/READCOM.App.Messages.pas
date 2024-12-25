c725660952ba65594af40480a29e5797473b4453
birbilis <birbilis@zoomicon.com>
2024-12-26 01:14:10 +0200

    - at READCOM.App.Messages converted URL_HELP, URL_READCOM, STR_APP_TITLE resourcestrings to vars so that custom apps can modify them at runtime

--- App/READCOM.App.Messages.pas
+++ App/READCOM.App.Messages.pas
@@ -2,12 +2,16 @@ unit READCOM.App.Messages;
 
 interface
 
+//Localizable
 resourcestring
-  URL_HELP = 'https://github.com/Zoomicon/READCOM_App/wiki';
-  URL_READCOM = 'https://www.read-com-eu.uma.es';
-  STR_APP_TITLE = 'READ-COM: Reading Communities';
   STR_COMPATIBILITY_MODE = '[Compatibility mode]';
   ERR_DOWNLOAD = 'Download failed (%s)';
+
+//Replaceable by custom apps
+var
+  URL_HELP: String = 'https://github.com/Zoomicon/READCOM_App/wiki';
+  URL_READCOM: String = 'https://www.read-com-eu.uma.es';
+  STR_APP_TITLE:String = 'READ-COM: Reading Communities';
 
 implementation
 
