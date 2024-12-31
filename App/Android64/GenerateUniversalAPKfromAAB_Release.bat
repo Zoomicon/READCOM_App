0a8de915ae9918068287c9269c418410a7de6dc4
birbilis <birbilis@zoomicon.com>
2024-12-31 10:50:37 +0200

    - SKIA related files (e.g. DLLs) autoadded to deployment info - Added notes to universal .APK from .AAB extraction batch files that output .APK needs its datetime updated via "touch" cmd-line tool (Delphi includes one) - Updated zoomicon.introspection.fmx.delphi BOSS package dependency to >=3.3.0

--- App/Android64/GenerateUniversalAPKfromAAB_Release.bat
+++ App/Android64/GenerateUniversalAPKfromAAB_Release.bat
@@ -8,6 +8,6 @@ del READCOM_App.apks
 
 java -jar bundletool.jar build-apks --bundle=Release\READCOM_App\bin\READCOM_App.aab --output=READCOM_App.apks --overwrite --mode=universal --ks=%UserProfile%\OneDrive\Zoomicon\Android\Keystore.keystore --ks-pass=pass:%PWD_KEYSTORE% --ks-key-alias=readcom --key-pass=pass:%PWD_KEY%
 
-echo Open the .apks with a ZIP extractor (or rename to .zip) and get the universal .apk from there (should rename to READCOM_App.universal.apk)
+echo Open the .apks with a ZIP extractor (or rename to .zip) and get the universal .apk from there (should rename to READCOM_App.universal.apk). Can also call touch on the .apk filename else it has very old/default date
 
 @pause
