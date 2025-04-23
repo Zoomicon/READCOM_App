4aca2a9b1b1b711b116b9d5ecca8c1cb502b24fe
birbilis <birbilis@zoomicon.com>
2025-04-23 03:11:27 +0300

    - Moved StoryItems and Options to READCOM.Core.Delphi BOSS package

--- App/READCOM_App.dpr
+++ App/READCOM_App.dpr
@@ -7,10 +7,9 @@ program READCOM_App;
 
   {$R *.dres} //for Windows resources added via Resources and Images (includes 'Default.readcom' startup story document)
 
-  {$region 'Used units' ---------------------------------------------------------} //Note: D12.2 can't fold/expand regions in .dpr files
+  {$region 'Used units' ---------------------------------------------------------} //Note: D12.3 can't fold/expand regions in .dpr files
   uses
   System.StartUpCopy,
-  Zoomicon.Manipulation.FMX.CustomManipulator in 'modules\zoomicon.manipulation.fmx.delphi\Source\Zoomicon.Manipulation.FMX.CustomManipulator.pas' {CustomManipulator: TFrame},
   Zoomicon.Media.FMX.ModalFrame in 'modules\zoomicon.media.fmx.delphi\Source\Zoomicon.Media.FMX.ModalFrame.pas' {ModalFrame: TFrame},
   READCOM.Views.Dialogs.About in 'Views\Dialogs\READCOM.Views.Dialogs.About.pas' {AboutFrame: TFrame},
   READCOM.Views.HUD in 'Views\READCOM.Views.HUD.pas' {StoryHUD: TFrame},
@@ -18,14 +17,7 @@ program READCOM_App;
   READCOM.App.Messages in 'READCOM.App.Messages.pas',
   READCOM.App.Main in 'READCOM.App.Main.pas',
   READCOM.Resources.Icons in 'modules\readcom.core.delphi\Source\Resources\READCOM.Resources.Icons.pas' {Icons: TDataModule},
-  READCOM.Resources.Themes in 'modules\readcom.core.delphi\Source\Resources\READCOM.Resources.Themes.pas' {Themes: TDataModule},
-  READCOM.Views.StoryItems.StoryItem in 'Views\StoryItems\READCOM.Views.StoryItems.StoryItem.pas' {StoryItem: TFrame},
-  READCOM.Views.StoryItems.ImageStoryItem in 'Views\StoryItems\READCOM.Views.StoryItems.ImageStoryItem.pas' {ImageStoryItem: TFrame},
-  READCOM.Views.StoryItems.TextStoryItem in 'Views\StoryItems\READCOM.Views.StoryItems.TextStoryItem.pas' {TextStoryItem: TFrame},
-  READCOM.Views.StoryItems.AudioStoryItem in 'Views\StoryItems\READCOM.Views.StoryItems.AudioStoryItem.pas' {AudioStoryItem: TFrame},
-  READCOM.Views.Options.StoryItemOptions in 'Views\Options\READCOM.Views.Options.StoryItemOptions.pas' {StoryItemOptions: TFrame},
-  READCOM.Views.Options.TextStoryItemOptions in 'Views\Options\READCOM.Views.Options.TextStoryItemOptions.pas' {TextStoryItemOptions: TFrame},
-  READCOM.Views.Options.ImageStoryItemOptions in 'Views\Options\READCOM.Views.Options.ImageStoryItemOptions.pas' {ImageStoryItemOptions: TFrame};
+  READCOM.Resources.Themes in 'modules\readcom.core.delphi\Source\Resources\READCOM.Resources.Themes.pas' {Themes: TDataModule};
   {$endregion}
 
   {$R *.res} //for Windows App metadata defined via Project Options (App Icon, Versioning Info)
