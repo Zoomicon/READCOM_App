389e6196b3c1bafbc84aa2c3a04487a31801f974
birbilis <birbilis@zoomicon.com>
2025-08-03 04:40:43 +0300

    using READCOM.Core 8.1 not trying to do fullscreen on StoryFormReady

--- App/READCOM.App.EventOverrides.pas
+++ App/READCOM.App.EventOverrides.pas
@@ -14,14 +14,23 @@ interface
 
 implementation
   uses
-    System.SysUtils; //for FreeAndNil
+    System.SysUtils, //for FreeAndNil
+    FMX.Forms; //for Application
 
   procedure TEventHandlers.StoryFormReady(const StoryForm: TStoryForm);
   begin
-    (* //uncomment to start with UI buttons hidden (apart from menu toggle button that shows/hides the rest)
+    //Application.MainForm := StoryForm; //do this since it may have not been done yet and code we call may expect it to have been done
+
+    (* //NOT USES, SHOWING MENU AND NAVIGATION BUTTONS BY DEFAULT
+    //Untoggle actionMenu / hide layoutButtons by default (assuming content has its own navigation StoryItems with UrlAction set)
     StoryForm.HUD.actionMenu.Checked := false; //don't just do StoryForm.HUD.layoutButtons.Visible := false since that would make the menu toggle button unsync
     StoryForm.HUD.layoutButtons.Visible := false;
     *)
+
+    //Use fullscreen by default
+    {$IF Defined(Android)} //FullScreen still has issues on Windows (e.g. color popups don't show), only do on Android
+    //StoryForm.HUD.Fullscreen := true; //TODO: doing this here on Android seems to result in blank screen
+    {$ENDIF}
   end;
 
   procedure TEventHandlers.StoryLoaded(const Story: TStory);
