3dfa61e76cf0f131c9f899269f1509dd036fba22
birbilis <birbilis@zoomicon.com>
2025-10-26 17:52:33 +0200

    - Added BOSS_install.bat (deletes modules folder and does BOSS install) and BOSS_update.bat (just does BOSS update) - changed version (including in boss.json) to 0.7.9 - using READCOM.Core.Delphi BOSS package 8.3.0

---
+++ BOSS_install.bat
@@ -0,0 +1,47 @@
+@echo off
+setlocal
+
+:: Save the original working directory
+set "ORIG_DIR=%CD%"
+
+call :RunBossInstall "App"
+
+pause
+exit /b
+
+:: === Subroutine ===
+:RunBossInstall
+set "TARGET=%~1"
+set "JUNCTION=C:\myTMP"
+
+::-- Note: use a really short path for the JUNCTION
+
+:: Always return to original dir before creating junction
+cd /d "%ORIG_DIR%"
+
+echo.
+echo Creating junction: %JUNCTION% -^> %TARGET%
+mklink /J "%JUNCTION%" "%TARGET%" > nul
+if errorlevel 1 (
+    echo Failed to create junction for %TARGET%
+    exit /b 1
+)
+
+cd /d "%JUNCTION%"
+
+::-- Note: to change Delphi compiler use:
+::-- boss config delphi list
+::-- boss config delphi use X -g
+::-- where X is the compiler you want from the list shown. The -g makes it remembered globally
+::-- in that case make sure old modules folder gets removed, then do boss install
+
+echo Removing old modules folder
+rd /s/q modules
+
+echo Running boss install in %JUNCTION%...
+boss install
+
+echo Removing junction: %JUNCTION%
+rmdir "%JUNCTION%"
+
+exit /b
---
+++ BOSS_update.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal
+
+:: Save the original working directory
+set "ORIG_DIR=%CD%"
+
+call :RunBossUpdate "App"
+
+pause
+exit /b
+
+:: === Subroutine ===
+:RunBossUpdate
+set "TARGET=%~1"
+set "JUNCTION=C:\myTMP"
+
+::-- Note: use a really short path for the JUNCTION
+
+:: Always return to original dir before creating junction
+cd /d "%ORIG_DIR%"
+
+echo.
+echo Creating junction: %JUNCTION% -^> %TARGET%
+mklink /J "%JUNCTION%" "%TARGET%" > nul
+if errorlevel 1 (
+    echo Failed to create junction for %TARGET%
+    exit /b 1
+)
+
+cd /d "%JUNCTION%"
+echo Running boss update in %JUNCTION%...
+boss update
+
+echo Removing junction: %JUNCTION%
+rmdir "%JUNCTION%"
+
+exit /b
