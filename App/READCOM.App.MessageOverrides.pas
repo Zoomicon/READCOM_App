e48d3be515a8d0492130c41dcb1a2c2407b23659
birbilis <birbilis@zoomicon.com>
2026-01-06 22:54:56 +0200

    - using READCOM.Core.Delphi 18.0

--- App/READCOM.App.MessageOverrides.pas
+++ App/READCOM.App.MessageOverrides.pas
@@ -11,8 +11,11 @@ implementation
 initialization
   //Note: in a custom app would set (override default values) the following:
 
+  //STR_APP_TITLE := 'READ-COM: Reading Communities';
+
   //URL_HELP := 'https://github.com/Zoomicon/READCOM_App/wiki';
   //URL_READCOM := 'https://www.read-com-eu.uma.es';
-  //STR_APP_TITLE := 'READ-COM: Reading Communities';
+
+  //URL_GALLERY := 'https://raw.githubusercontent.com/Zoomicon/READCOM_Gallery/master/Gallery/';
 
 end.
